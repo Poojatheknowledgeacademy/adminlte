9518111b92c34b8c4717d7061b3d78e06aa44385
Sweety Kumari <Sweety.Kumari@theknowledgeacademy.com>
2023-09-27 10:49:15 +0530

    sweety changes file name

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,49 +1,39 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Http\Requests\StoreBlogRequest;
 use App\Http\Requests\EditBlogRequest;
 use App\Models\Blog;
-use App\Models\category;
+use App\Models\Tag;
+use App\Models\Category;
 use Illuminate\Http\Request;
-
-class BlogController extends Controller
-{
+class BlogController extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index() {
         $blog = Blog::paginate(2);
         return view('blog.list', compact('blog'));
     }
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        ////
-        $category = category::all();
-        return view('blog.createblog', compact('category'));
+    public function create() {
+        $category = Category::all();
+        $tags = Tag::where('is_active', 1)->get();
+        return view('blog.create', compact('category', 'tags'));
     }
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreBlogRequest $request)
-    {
-       // print_r($request->all());
-       // die();
+    public function store(StoreBlogRequest $request) {
         $image1 = $request->file('featured_img1');
         $image1name = time() . '_' . $image1->getClientOriginalName();
         $image1location = 'Images/featureimage1/';
-        // Upload file
         $image1->move($image1location, $image1name);
         $filepath1 = $image1location . $image1name;
         $image2 = $request->file('featured_img2');
         $image2name = time() . '_' . $image2->getClientOriginalName();
         $image2location = 'Images/featureimage2/';
-        // Upload file
         $image2->move($image2location, $image2name);
         $filepath2 = $image2location . $image2name;
         $id = auth()->user()->id;
@@ -52,36 +42,29 @@ class BlogController extends Controller
         } else {
             $popular = '0';
         }
-
         $data = $request->validated();
-        $blog= Blog::create(array_merge($request->all(), ["created_by" => $id, "is_popular" => $popular, "featured_img1" => $filepath1, "featured_img2" => $filepath2]));
-        $blog->slugs()->create([
-            'slug' => $request->slug,
-        ]);
+        $blog = Blog::create(array_merge($request->all(), ["created_by" => $id, "is_popular" => $popular, "featured_img1" => $filepath1, "featured_img2" => $filepath2]));
+        $blog->slugs()->create(['slug' => $request->slug, ]);
         return redirect()->route('blogs.index')->with('success', 'Blog Created Successfully.');
     }
     /**
      * Display the specified resource.
      */
-    public function show(Blog $blog)
-    {
+    public function show(Blog $blog) {
         //
 
     }
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Blog $blog)
-    {
-        //
+    public function edit(Blog $blog) {
         $category = category::all();
         return view('blog.edit', compact('blog', 'category'));
     }
     /**
      * Update the specified resource in storage.
      */
-    public function update(EditBlogRequest $request, Blog $blog)
-    {
+    public function update(EditBlogRequest $request, Blog $blog) {
         if ($request->is_popular == 'on') {
             $popular = '1';
         } else {
@@ -95,10 +78,7 @@ class BlogController extends Controller
         $blog->summary = $request->summary;
         $blog->author_name = $request->author_name;
         $blog->added_date = $request->added_date;
-
-        $blog->slugs()->updateOrCreate(
-            ['slug' => $request->slug]
-        );
+        $blog->slugs()->updateOrCreate(['slug' => $request->slug]);
         if ($request->file('featured_img1')) {
             $featureimage1location = 'Images/featureimage1/';
             if (!empty($blog->featured_img1)) {
@@ -107,7 +87,6 @@ class BlogController extends Controller
             $feature_image1 = $request->file('featured_img1');
             $featureimg1_name = time() . '_' . $feature_image1->getClientOriginalName();
             $feature_image1->move($featureimage1location, $featureimg1_name);
-            // $blog->update(["featured_img1" => $featureimage1location . $featureimg1_name]);
             $blog->featured_img1 = $featureimage1location . $featureimg1_name;
         }
         if ($request['removefeature1txt'] != null) {
@@ -121,7 +100,6 @@ class BlogController extends Controller
             $feature_image2 = $request->file('featured_img2');
             $featureimg2_name = time() . '_' . $feature_image2->getClientOriginalName();
             $feature_image2->move($featureimage2location, $featureimg2_name);
-            //$blog->update(["featured_img2" => $featureimage2location . $featureimg2_name]);
             $blog->featured_img2 = $featureimage2location . $featureimg2_name;
         }
         if ($request['removefeature2txt'] != null) {
@@ -129,14 +107,11 @@ class BlogController extends Controller
         }
         $blog->save();
         return redirect()->route('blogs.index')->with('success', 'Blog Updated Successfully');
-        // $blog->update(["category_id" => $request->category_id, "title" => $request->title, "short_description" => $request->short_description, "summary" => $request->summary, "author_name" => $request->author_name, "added_date" => $request->added_date, "is_popular" => $popular,"featured_img1"=>$featureimage1 ]);
-
     }
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Blog $blog)
-    {
+    public function destroy(Blog $blog) {
         //
         $post = blog::find($blog->id);
         $post->delete();
---
+++ resources/views/blog/create.blade.php
@@ -0,0 +1,189 @@
+@extends('layouts.app')
+@section('content')
+    {{-- @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif --}}
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1>Blog</h1>
+                </div>
+                <div class="col-sm-12">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Blog</a></li>
+                        <li class="breadcrumb-item active">Create Blog</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Create Blog</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('blogs.store') }}" enctype="multipart/form-data">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                <div class="form-group">
+                                    <label>Category</label>
+                                    <select name="category_id" class="form-control select2bs4 @error('category_id') is-invalid @enderror">
+                                        <option value="">Select a category</option>
+                                        @foreach ($category as $categories)
+                                            <option value="{{ $categories->id }}">{{ $categories->name }}</option>
+
+                                        @endforeach
+
+                                    </select>
+                                    @error('category_id')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Slug</label>
+                                    <input type="text" class="form-control @error('slug') is-invalid @enderror"
+                                        name="slug" value="{{ old('slug') }}">
+                                    @error('slug')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label>Title</label>
+                                    <input type="text" class="form-control @error('title') is-invalid @enderror"
+                                        name="title" value="{{ old('title') }}">
+                                    @error('title')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label>Short description</label>
+                                    <input type="text"
+                                        class="form-control @error('short_description') is-invalid @enderror"
+                                        name="short_description" value="{{ old('short_description') }}">
+                                    @error('short_description')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label>Summary</label>
+                                    {{-- <textarea id="summernote">
+                                        Place <em>some</em> <u>text</u> <strong>here</strong>
+                                      </textarea> --}}
+                                    <textarea id="summernote" class="summernote @error('summary') is-invalid @enderror" name="summary">{{ old('summary') }}</textarea>
+                                    @error('summary')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label>Featured image1</label>
+                                    <input type="file" class="form-control @error('featured_img1') is-invalid @enderror"
+                                        name="featured_img1" value="{{ old('featured_img1') }}">
+                                    @error('featured_img1')
+                                        <div class="error invalid-feedback d-block">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <label>Featured image2</label>
+
+                                    <div class="custom-file">
+                                        <input type="file"
+                                            class="form-control @error('featured_img2') is-invalid @enderror"
+                                            name="featured_img2" value="{{ old('featured_img2') }}">
+                                    </div>
+                                    @error('featured_img2')
+                                        <div class="error invalid-feedback d-block">{{ $message }}</div>
+                                    @enderror
+
+                                </div>
+                                <div class="form-group">
+                                    <label>Author Name</label>
+                                    <div class="input-group">
+                                        <input type="text"
+                                            class="form-control @error('author_name') is-invalid @enderror"
+                                            name="author_name" value="{{ old('author_name') }}">
+                                        @error('author_name')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Date</label>
+                                    <div class="input-group">
+                                        <input type="date" class="form-control @error('added_date') is-invalid @enderror"
+                                            name="added_date" value="{{ old('added_date') }}">
+                                        @error('added_date')
+                                            <div class="invalid-feedback">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                {{-- <div class="form-group">
+                                    <label>Tag</label>
+                                    <div style="width: 50%">
+                                        <select id="pieces" nam="tag" class="form-control select2bs4 @error('tag') is-invalid @enderror" multiple>
+                                        </select>
+                                      </div>
+
+
+                                </div> --}}
+                                <div class="form-group">
+
+                                    <label>Tags</label>
+
+                                    <select class="select2" name="tagname1" multiple="multiple"  style="width: 100%;" id="pieces">
+                                        <option value="">select tag</option>
+                                        @foreach ($tags as $tag)
+
+                                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+
+
+
+                                    @endforeach
+
+                                    </select>
+
+                                </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-switch">
+                                        <input type="checkbox" class="custom-control-input" id="customSwitch1"
+                                            name="is_popular">
+                                        <label class="custom-control-label" for="customSwitch1">Popular</label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-switch">
+                                        <input type="checkbox" class="custom-control-input" id="customSwitch2"
+                                            name="is_active" checked>
+                                        <label class="custom-control-label" for="customSwitch2">Active</label>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary">Create</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    </div>
+@endsection
+
+
+
