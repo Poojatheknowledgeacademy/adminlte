c7d8a2edb839a503087e6eb39a1dfd9eb2081b74
25dhara <dhara.patel@theknowledgeacademy.com>
2023-10-30 19:17:43 +0530

    role timeline

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,19 +2,12 @@
 
 namespace App\Providers;
 
-use App\Models\Faq;
-use App\Models\Tag;
 use App\Models\Blog;
 use App\Models\Role;
-<<<<<<< HEAD
 use App\Models\User;
-=======
->>>>>>> c59aee85bcf1cac89aaea267e8e06f8ddddaaef8
 use App\Models\Topic;
 use App\Models\Course;
-use App\Models\Module;
 use App\Models\Category;
-<<<<<<< HEAD
 use App\Models\Permission;
 use App\Models\Topicdetail;
 use App\Observers\BlogObserver;
@@ -25,19 +18,7 @@ use App\Observers\CourseObserver;
 use App\Observers\CategoryObserver;
 use App\Observers\PermissionObserver;
 use App\Observers\TopicdetailObserver;
-=======
-use App\Models\Coursedetail;
-use App\Observers\FaqObserver;
-use App\Observers\TagObserver;
-use App\Observers\BlogObserver;
-use App\Observers\RoleObserver;
-use App\Observers\TopicObserver;
-use App\Observers\CourseObserver;
-use App\Observers\CategoryObserver;
-use App\Observers\ModuleObserver;
->>>>>>> c59aee85bcf1cac89aaea267e8e06f8ddddaaef8
 use Illuminate\Auth\Events\Registered;
-use App\Observers\CoursedetailObserver;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -63,18 +44,9 @@ class EventServiceProvider extends ServiceProvider
         Course::observe(CourseObserver::class);
         Blog::observe(BlogObserver::class);
         Category::observe(CategoryObserver::class);
-<<<<<<< HEAD
         User::observe(UserObserver::class);
         Topicdetail::observe(TopicdetailObserver::class);
         Permission::observe(PermissionObserver::class);
-       
-=======
-        Tag::observe(TagObserver::class);
-        Coursedetail::observe(CoursedetailObserver::class);
-        Faq::observe(FaqObserver::class);
-        Role::observe(RoleObserver::class);
-        Module::observe(ModuleObserver::class);
->>>>>>> c59aee85bcf1cac89aaea267e8e06f8ddddaaef8
     }
 
     /**
